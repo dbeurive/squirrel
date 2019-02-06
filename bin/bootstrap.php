50174c815651f5d4fa89ffab1851f5a59ca087b9
Denis BEURIVE <dbeurive.github@gmail.com>
2019-02-06 12:57:10 +0100

    Produce one script per action rather that a script that implements all the actions

---
+++ bin/bootstrap.php
@@ -0,0 +1,228 @@
+<?php
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+use League\CLImate\CLImate;
+use dbeurive\Log\Logger;
+use dbeurive\Squirrel\Configuration;
+
+// Path to the default configuration file.
+define('DEFAULT_CONFIGURATION', __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'squirrel.json');
+
+// Definition for the command line option that represents the path to the configuration file.
+define('CLO_CONF', 'config');
+
+// Definition for the command line option that represents the verbosity level.
+define('CLO_VERBOSE', 'verbose');
+
+// Common specification for the command line parser.
+define('CLI_COMMON_CONFIGURATION', array(
+    CLO_CONF => array(
+        'prefix'       => 'c',
+        'longPrefix'   => 'config',
+        'description'  => 'Path to the configuration file.',
+        'defaultValue' => DEFAULT_CONFIGURATION
+    ),
+    CLO_VERBOSE => array(
+        'prefix'       => 'v',
+        'longPrefix'   => 'verbose',
+        'description'  => 'Activate the verbose mode.',
+        'noValue'      => true
+    )
+));
+
+
+/**
+ * Class Environment
+ *
+ * This class represents the "outside" local environment of the script.
+ * The script interacts with the local environment through this class.
+ */
+class Environment {
+
+    /** @var CLImate */
+    static private $__climate;
+    /** @var bool */
+    static private $__cloVerbose;
+    /** @var string */
+    static private $__cloConfiguration;
+    /** @var string */
+    static private $__logPath;
+    /** @var Configuration */
+    static private $__configuration;
+    /** @var Logger */
+    static private $__logger;
+
+    /**
+     * Initialise the environment container.
+     * @param array $in_specific_args Climate specifications for the specific script.
+     * @throws Exception
+     */
+    static public function init(array $in_specific_args)
+    {
+        self::$__climate = new CLImate();
+        self::$__climate->arguments->add(array_merge(CLI_COMMON_CONFIGURATION, $in_specific_args));
+
+        // Parse the command line.
+        try {
+            self::$__climate->arguments->parse();
+        } catch (\Exception $e) {
+            self::fatal_error($e->getMessage());
+        }
+
+        self::$__cloConfiguration = self::$__climate->arguments->get(CLO_CONF);
+        self::$__cloVerbose = self::$__climate->arguments->get(CLO_VERBOSE);
+
+        // Load the configuration. This action may throw an exception.
+        try {
+            self::$__configuration = new Configuration(self::$__cloConfiguration, realpath(dirname(self::$__cloConfiguration)));
+        } catch (\Exception $e) {
+            self::fatal_error($e->getMessage());
+        }
+
+        // Open the LOG file.
+        $timestamp = strftime('%Y%m%d');
+        $log = self::$__configuration->getLog();
+
+        if ($log->fileTimestamped()) {
+            self::$__logPath = sprintf('%s%s%s-%s', $log->getDirectory(), DIRECTORY_SEPARATOR, $timestamp, $log->getName());
+        } else {
+            self::$__logPath = sprintf('%s%s%s', $log->getDirectory(), DIRECTORY_SEPARATOR, $log->getName());
+        }
+
+        self::$__logger = new Logger(self::$__logPath, self::$__configuration->getLog()->getLevel());
+    }
+
+    /**
+     * Return the object that contains the loaded configuration.
+     * @return Configuration The loaded configuration.
+     */
+    static public function getConfiguration() {
+       return self::$__configuration;
+    }
+
+    /**
+     * Test whether the verbosity mode is activated or not.
+     * @return bool If the verbosity mode is activated, then the method returns the value true.
+     *         Otherwise, it returns the value false.
+     */
+    static public function getCloVerbose() {
+        return self::$__cloVerbose;
+    }
+
+    /**
+     * Return the path to the configuration file that has been loaded.
+     * @return string The configuration file that has been loaded.
+     */
+    static public function getCloConfigurationPath() {
+        return self::$__cloConfiguration;
+    }
+
+    /**
+     * Return the value of a command line option identified by its name.
+     * @param string $in_name The name of the command line option.
+     * @return mixed the value of a command line option.
+     */
+    static public function getCloValue($in_name) {
+        return self::$__climate->arguments->get($in_name);
+    }
+
+    /**
+     * Return the value of a command line argument identified by its name.
+     * @param string $in_name The name of the command line argument.
+     * @return mixed the value of a command line argument.
+     */
+    static public function getClaValue($in_name) {
+        return self::$__climate->arguments->get($in_name);
+    }
+
+    /**
+     * Return the path to the LOG file.
+     * @return string The path to the LOG file.
+     */
+    static public function getLogPath() {
+       return self::$__logPath;
+    }
+
+    /**
+     * Return the logger.
+     * @return Logger The looger.
+     */
+    static public function getLogger() {
+       return self::$__logger;
+    }
+
+    /**
+     * Report a debug message.
+     * @param string $in_message The information to report.
+     * @throws Exception
+     */
+    static public function debug($in_message) {
+        self::$__logger->debug($in_message);
+        if (self::$__cloVerbose) {
+            self::$__climate->lightGray($in_message);
+        }
+    }
+
+    /**
+     * Report an information.
+     * @param string $in_message The information to report.
+     * @throws Exception
+     */
+    static public function info($in_message) {
+        self::$__logger->info($in_message);
+        if (self::$__cloVerbose) {
+            self::$__climate->out($in_message);
+        }
+    }
+
+    /**
+     * Report a successful operation.
+     * @param string $in_message The information to report.
+     * @throws Exception
+     */
+    static public function success($in_message) {
+        self::$__logger->success($in_message);
+        if (self::$__cloVerbose) {
+            self::$__climate->green($in_message);
+        }
+    }
+
+    /**
+     * Report a warning.
+     * @param string $in_message The information to report.
+     * @throws Exception
+     */
+    static public function warning($in_message) {
+        self::$__logger->warning($in_message);
+        if (self::$__cloVerbose) {
+            self::$__climate->yellow($in_message);
+        }
+    }
+
+
+    /**
+     * Report an error.
+     * @param string $in_message Message that describes the error.
+     * @throws Exception
+     */
+    static public function error($in_message) {
+        self::$__logger->error($in_message);
+        if (self::$__cloVerbose) {
+            self::$__climate->red($in_message);
+        }
+    }
+
+    /**
+     * Report a fatal error.
+     * @param string $in_message Message that describes the error.
+     * @throws Exception
+     */
+    static public function fatal($in_message) {
+        self::$__logger->fatal($in_message);
+        if (self::$__cloVerbose) {
+            self::$__climate->lightRed(sprintf('FATAL ERROR: %s', $in_message));
+        }
+        exit(1);
+    }
+}
+
---
+++ bin/squirrel-exec.php
@@ -0,0 +1,164 @@
+<?php
+
+include __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
+
+use dbeurive\Squirrel\FileScanner;
+use dbeurive\Squirrel\Destination;
+use dbeurive\Squirrel\BackupStatusManager;
+
+define('CLA_TASK', 'task');
+define('ARG_LIST_TASKS', 'list_tasks');
+define('ARG_LIST_DESTINATIONS', 'list_destinations');
+
+define('CLI_SPECIFIC_CONFIGURATION', array(
+    CLA_TASK => array(
+        'description'  => 'Name of the task to perform.',
+        'required'     => true
+    ),
+));
+
+// ---------------------------------------------
+// Initialise the environment.
+// ---------------------------------------------
+
+Environment::init(CLI_SPECIFIC_CONFIGURATION);
+$cla_task = Environment::getClaValue(CLA_TASK);
+$config = Environment::getConfiguration();
+
+if (! $config->isTaskConfigured($cla_task)) {
+    Environment::fatal(sprintf('Unknown task "%s".', $cla_task));
+}
+
+// ---------------------------------------------
+// Get the task to execute.
+// ---------------------------------------------
+
+$task = $config->getTask($cla_task);
+
+Environment::info(sprintf('Starting task "%s"', $task));
+Environment::info(sprintf('LOG file is: %s', Environment::getLogPath()));
+Environment::info(sprintf('File ID = "%s".', $task->getFileId()));
+Environment::info(sprintf('Local input directory = "%s".', $task->getLocalInputDirectory()));
+Environment::info(sprintf('Local done directory = "%s".', $task->getLocalDoneDirectory()));
+
+// ---------------------------------------------
+// Lists the files to backup.
+// ---------------------------------------------
+
+$files_to_backup = array();
+$file_scanner = new FileScanner($task->getLocalInputDirectory(), $task->getFileId());
+
+try {
+    $files_to_backup = $file_scanner->getFiles();
+    Environment::info(sprintf('Number of files to backup: %d', count($files_to_backup)));
+    Environment::debug(sprintf('List of local files to backup: %s', implode(', ', $files_to_backup)));
+} catch (\Exception $e) {
+    Environment::fatal(sprintf('Cannot get the list of files to backup! %s', $e->getMessage()));
+}
+
+$error_message = null;
+
+// ---------------------------------------------
+// Send files.
+// ---------------------------------------------
+
+$backup_status = new BackupStatusManager($files_to_backup);
+
+if (count($files_to_backup)) {
+
+    Environment::info("Backup files.");
+    /** @var Destination $_destination */
+    foreach ($task->getDestinations() as $_destination) {
+
+        Environment::info(sprintf('Open a connexion to "%s"', $_destination->getName()));
+        if (false === $_destination->reach($error_message)) {
+            Environment::error(sprintf('Fail to open a connexion to "%s": %s', $_destination, $error_message));
+            $backup_status->setErrors($files_to_backup);
+            continue;
+        }
+
+        if (false === $_destination->createPath($error_message)) {
+            Environment::error(sprintf('Fail to to create the directory "%s" on the destination "%s": %s', $_destination->getPath(), $_destination, $error_message));
+            $backup_status->setErrors($files_to_backup);
+            continue;
+        }
+
+        foreach ($files_to_backup as $_local_file_path) {
+            Environment::info(sprintf('Send "%s"', basename($_local_file_path)));
+            if (false === $_destination->send($_local_file_path, $error_message)) {
+                Environment::error(sprintf('Fail to transfer the file "%s" to "%s": %s', $_local_file_path, $_destination, $error_message));
+                $backup_status->setError($_local_file_path);
+                continue;
+            }
+            Environment::success(sprintf('File "%s" successfully transferred to "%s".', basename($_local_file_path), $_destination));
+        }
+
+        Environment::debug(sprintf('Disconnect from "%s".', $_destination->getName()));
+        $_destination->leave();
+    }
+
+    Environment::info("Moving backed up files to the \"done\" directory.");
+    foreach ($files_to_backup as $_local_file_path) {
+
+        if ($backup_status->isError($_local_file_path)) {
+            Environment::info(sprintf('Do not delete "%s".', basename($_local_file_path)));
+            continue;
+        }
+
+        Environment::info(sprintf('Move "%s"', basename($_local_file_path)));
+        if (false === $task->done($_local_file_path, $error_message)) {
+            Environment::error(sprintf('Fail to move "%s" to "%s": %s', $_local_file_path, $task->getLocalDoneDirectory(), $error_message));
+            continue;
+        }
+        Environment::success(sprintf('File "%s" successfully moved to "%s".', basename($_local_file_path), $task->getLocalDoneDirectory()));
+    }
+}
+
+// ---------------------------------------------
+// Remove expired files.
+// ---------------------------------------------
+
+/** @var Destination $_destination */
+foreach ($task->getDestinations() as $_destination) {
+
+    Environment::info(sprintf('Open a connexion to "%s"', $_destination->getName()));
+    if (false === $_destination->reach($error_message)) {
+        Environment::error(sprintf('Fail to open a connexion to "%s": %s', $_destination->getName(), $error_message));
+        continue;
+    }
+
+    $expired = array();
+    Environment::info(sprintf('Get the list of files to delete on "%s"', $_destination));
+    if (false === $files = $_destination->inventory($task->getKeepCount(),$expired, $error_message)) {
+        Environment::error(sprintf('Fail to get the list of remote files to delete on "%s": %s', $_destination, $error_message));
+        continue;
+    }
+
+    Environment::info(sprintf('Number of remote files to delete on "%s": %d', $_destination, count($expired)));
+    Environment::debug(sprintf('List of remote files to delete: %s', implode(', ', $expired)));
+
+    if (count($expired) > 0) {
+        foreach ($expired as $_file) {
+
+            Environment::info(sprintf('Del "%s" from "%s"', $_file, $_destination->getName()));
+            if (false === $_destination->remove($_file, $error_message)) {
+                Environment::error(sprintf('Fail to remove "%s" from "%s": %s', $_file, $_destination, $error_message));
+            } else {
+                Environment::success(sprintf('File "%s" successfully deleted from "%s".', $_file, $_destination));
+            }
+        }
+    } else {
+        Environment::info(sprintf('No file to delete on "%s".', $_destination));
+    }
+
+    Environment::debug(sprintf('Disconnect from "%s".', $_destination->getHost()));
+    $_destination->leave();
+}
+
+Environment::info('Done');
+
+
+
+
+
+
